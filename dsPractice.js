404977881608908b9acf6f5d83f09e68f6d243c3
Kevin Gallo <Gallok731@gmail.com>
2022-07-08 13:58:21 -0400

    Queues

--- dsPractice.js
+++ dsPractice.js
@@ -279,6 +279,7 @@ class DoubleLinkedList {
   }
 }
 
+// Stack and Queue Node
 class Node{
   constructor(val){
     this.val = val
@@ -346,6 +347,40 @@ class Stack{
     // this.size--
     // return first
   }
+}
+
+class Queue{
+  constructor(){
+    this.first = null
+    thus.last = null
+    this.size = 0
+  }
+  enqueue(val){
+    let newNode = new Node(val)
+    if(this.size === 0){
+      this.first = newNode
+      this.last = newNode
+    }else{
+      this.last.next = newNode
+      this.last = newNode
+    }
+    return ++this.size
+  }
+
+  dequeue(){
+    if(this.size === 0) return  null
+    let temp = this.first
+    if(this.size === 1){
+      this.first = null
+      this.last = null
+    }else{
+      this.first = temp.next
+      temp.next = null
+    }
+    this.size--
+    return temp
+  }
+
 }
 
 let Dlist = new DoubleLinkedList();
