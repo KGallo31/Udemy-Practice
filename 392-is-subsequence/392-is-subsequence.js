09ebdd320342f76d108808774d9da5aef0b11cc4
Kevin Gallo <49046277+KGallo31@users.noreply.github.com>
2022-08-19 13:40:50 -0400

    Time: 115 ms (13.90%), Space: 42.4 MB (22.69%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let pointer = 0
+    for(let char of t){
+        if(pointer === s.length) return true
+        if(char === s[pointer]) pointer++
+    }
+    return pointer === s.length
+    
+};
