c8319b3b233af4986eee934017e05666cf892cac
Kevin Gallo <49046277+KGallo31@users.noreply.github.com>
2022-08-23 12:26:00 -0400

    Time: 450 ms (16.46%), Space: 50.5 MB (95.21%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -3,10 +3,15 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if( n === 0 ) return false
-    while(n % 3 === 0){
-        n /= 3
-    }
+//     if( n === 0 ) return false
+//     while(n % 3 === 0){
+//         n /= 3
+//     }
     
-    return n === 1
+//     return n === 1
+    Math.abs(n)
+    if(n === 1) return true
+    if(n % 3 !== 0 || n===0) return false
+    return isPowerOfThree(n/3)
+    
 };
