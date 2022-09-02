761147ff3ed5d1ba864c0402fa301e7de03ce7d9
Kevin Gallo <49046277+KGallo31@users.noreply.github.com>
2022-09-02 12:41:57 -0400

    Time: 117 ms (7.92%), Space: 44.5 MB (5.74%) - LeetHub

---
+++ 100-same-tree/100-same-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    
+    let arr1 = []
+    let arr2 = []
+    
+    const dfs = (node,arr) => {
+        if(node === null) return arr.push("null")
+        arr.push(node.val)
+        return dfs(node.left,arr),dfs(node.right,arr)
+    }
+    dfs(p,arr1)
+    dfs(q,arr2)
+    if(arr1.length !== arr2.length) return false
+    for(let i = 0;i<arr1.length;i++){
+        console.log(`arr1: ${arr1[i]}, arr2: ${arr2[i]}`)
+        if(arr1[i] !== arr2[i]) return false
+    }
+    return true
+};
